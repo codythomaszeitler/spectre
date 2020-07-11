78c43149890afe972db8b9c130811ed828c6d6c6
Cody Zeitler <codyzeitler@Codys-MacBook-Pro.local>
2020-07-11 16:15:19 -0700

    Change build script to open website after deploymnet

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ expo build:web
 cd web-build
 aws s3 sync . s3://spectre.zsi --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 cd ..
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://spectre.zsi.s3-website-us-east-1.amazonaws.com/

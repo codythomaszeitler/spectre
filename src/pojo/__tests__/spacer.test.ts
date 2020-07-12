83c7e6c2f8000a9b37677759631d0158bdfd7997
Cody Zeitler <codyzeitler@Codys-MacBook-Pro.local>
2020-07-11 18:34:26 -0700

    Add spacer logic

---
+++ src/pojo/__tests__/spacer.test.ts
@@ -0,0 +1,106 @@
+import { Category } from "../category";
+import { Spacer } from "../spacer";
+
+describe("Spacer", () => {
+  let spacer: Spacer;
+
+  let spacers: Array<Spacer>;
+  let beforeCategorization: Category;
+  let afterCategorization: Category;
+
+  beforeEach(() => {
+    beforeCategorization = new Category("Before");
+    afterCategorization = new Category("After");
+    spacer = new Spacer(beforeCategorization, afterCategorization);
+    spacers = [];
+    spacers.push(spacer);
+  });
+
+  it("should be able to tell if a spacer is needed between two categories", () => {
+    expect(Spacer.containsSpacerBefore(spacers, afterCategorization)).toBe(
+      true
+    );
+    expect(Spacer.containsSpacerBefore(spacers, beforeCategorization)).toBe(
+      false
+    );
+
+    expect(Spacer.containsSpacerAfter(spacers, beforeCategorization)).toBe(
+      true
+    );
+    expect(Spacer.containsSpacerAfter(spacers, afterCategorization)).toBe(
+      false
+    );
+  });
+
+  it("should be able to tell if a spacer is needed at the beginning of the collection", () => {
+    const beginningSpacer = new Spacer(
+      Spacer.START_OF_CATEGORIES(),
+      afterCategorization
+    );
+    spacers = [];
+    spacers.push(beginningSpacer);
+
+    expect(Spacer.containsSpacerBefore(spacers, afterCategorization)).toBe(
+      true
+    );
+  });
+
+  it("should be able to tell if a spacer is needed at the end of the collection", () => {
+    const endSpencer = new Spacer(
+      beforeCategorization,
+      Spacer.END_OF_CATEGORIES()
+    );
+    spacers = [];
+    spacers.push(endSpencer);
+
+    expect(Spacer.containsSpacerAfter(spacers, beforeCategorization)).toBe(
+      true
+    );
+  });
+
+  it("should return false if the category is empty for containsSpacerBefore", () => {
+    expect(Spacer.containsSpacerBefore(spacers, null)).toBe(false);
+  });
+
+  it("should return false if the category is empty for containsSpacerAfter", () => {
+    expect(Spacer.containsSpacerAfter(spacers, null)).toBe(false);
+  });
+
+  it("should return false if spacers are empty for before call", () => {
+    const category = new Category("Test");
+    const spacers = Array<Spacer>();
+    expect(Spacer.containsSpacerBefore(spacers, category)).toBe(false);
+  });
+
+  it("should return false if spacers are empty for after call", () => {
+    const category = new Category("Test");
+    const spacers = Array<Spacer>();
+    expect(Spacer.containsSpacerAfter(spacers, category)).toBe(false);
+  });
+
+  it("should throw an exception if a null before category is given", () => {
+    let caughtException = null;
+    try {
+      new Spacer(null, afterCategorization);
+    } catch (e) {
+      caughtException = e;
+    }
+
+    expect(caughtException.message).toBe(
+      "Cannot create a spacer with a null before category"
+    );
+  });
+
+  it("should throw an exception if a null after category is given", () => {
+    let caughtException = null;
+    try {
+      new Spacer(beforeCategorization, null);
+    } catch (e) {
+      caughtException = e;
+    }
+
+    expect(caughtException.message).toBe(
+      "Cannot create a spacer with a null after category"
+    );
+  });
+});
---
+++ src/pojo/spacer.ts
@@ -0,0 +1,65 @@
+import { Category } from "./category";
+
+export class Spacer {
+  before: Category;
+  after: Category;
+
+  constructor(before: Category, after: Category) {
+    if (!before) {
+      throw new Error("Cannot create a spacer with a null before category");
+    }
+
+    if (!after) {
+      throw new Error("Cannot create a spacer with a null after category");
+    }
+
+    this.before = before.copy();
+    this.after = after.copy();
+  }
+
+  static START_OF_CATEGORIES() {
+    return new Category("___UNIQUE STRING FOR START OF CATEGORIES___");
+  }
+
+  static END_OF_CATEGORIES() {
+    return new Category("___UNIQUE STRING FOR END OF CATEGORIES___");
+  }
+
+  static containsSpacerBefore(spacers: Spacer[], category: Category) {
+    if (!category) {
+      return false;
+    }
+
+    let containsSpacerBefore = false;
+
+    for (let i = 0; i < spacers.length; i++) {
+      const spacer = spacers[i];
+
+      if (spacer.after.equals(category)) {
+        containsSpacerBefore = true;
+        break;
+      }
+    }
+
+    return containsSpacerBefore;
+  }
+
+  static containsSpacerAfter(spacers: Spacer[], category: Category) {
+    if (!category) {
+      return false;
+    }
+
+    let containsSpacerAfter = false;
+
+    for (let i = 0; i < spacers.length; i++) {
+      const spacer = spacers[i];
+
+      if (spacer.before.equals(category)) {
+        containsSpacerAfter = true;
+        break;
+      }
+    }
+
+    return containsSpacerAfter;
+  }
+}

aec593edd9a0100aeaf950849b98cc6829aada8c
Cody Zeitler <codyzeitler@Codys-MacBook-Pro.local>
2020-08-09 00:53:50 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf web-build
 expo build:web
 cd web-build
-aws s3 sync . s3://spectre.zsi --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync . s3://sceptresystem.org --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync . s3://sceptersystem.org --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 cd ..
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://spectre.zsi.s3-website-us-east-1.amazonaws.com/
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome sceptresystem.org
